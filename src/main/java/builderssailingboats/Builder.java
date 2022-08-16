ebe79390383ddadfd4031eae8518ce8ae9bf3bb5
PawelZw <nazwa_uzytkownika@users.noreply.github.com>
2022-08-16 11:36:06 +0200

    add SailingBoatBuilder

--- src/main/java/builderssailingboats/Builder.java
+++ src/main/java/builderssailingboats/Builder.java
@@ -1,4 +1,18 @@
 package builderssailingboats;
 
+import boats.HullMaterial;
+import boats.HullType;
+
 public interface Builder {
+
+    void setBrand(String brand);
+    void setModel(String model);
+    void setCountryOfOrigin(String countryOfOrigin);
+    void setLength(double length);
+    void setPrice(double price);
+    void setSeats(int seats);
+    void setProjectCategory(char projectCategory);
+    void setHullMaterial(HullMaterial hullMaterial);
+    void setHullType(HullType hullType);
+    void setWeight(double weight);
 }
--- src/main/java/builderssailingboats/SailingBoatBuilder.java
+++ src/main/java/builderssailingboats/SailingBoatBuilder.java
@@ -1,4 +1,74 @@
 package builderssailingboats;
 
+import boats.HullMaterial;
+import boats.HullType;
+import boats.Manual;
+
 public class SailingBoatBuilder implements Builder {
+
+    private String brand;
+    private String model;
+    private String countryOfOrigin;
+    private double length;
+    private double price;
+    private int seats;
+    private char projectCategory;
+    private HullMaterial hullMaterial;
+    private HullType hullType;
+
+
+    @Override
+    public void setBrand(String brand) {
+
+    }
+
+    @Override
+    public void setModel(String model) {
+
+    }
+
+    @Override
+    public void setCountryOfOrigin(String countryOfOrigin) {
+
+    }
+
+    @Override
+    public void setLength(double length) {
+
+    }
+
+    @Override
+    public void setPrice(double price) {
+
+    }
+
+    @Override
+    public void setSeats(int seats) {
+
+    }
+
+    @Override
+    public void setProjectCategory(char projectCategory) {
+
+    }
+
+    @Override
+    public void setHullMaterial(HullMaterial hullMaterial) {
+
+    }
+
+    @Override
+    public void setHullType(HullType hullType) {
+
+    }
+
+    @Override
+    public void setWeight(double weight) {
+
+    }
+    public Manual getResult() {
+        return new Manual(brand, model, countryOfOrigin, length, price, seats, projectCategory,
+                hullMaterial, hullType,);
+
+    }
 }
